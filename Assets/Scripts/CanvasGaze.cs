8bba0a74083f0db451ca5c621fbd33a902ea356b
jennifer hobbs <jen.g.hobbs@gmail.com>
2019-11-13 12:13:42 -0800

    gaze set up need to check on real computer

---
+++ Assets/Scripts/CanvasGaze.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasGaze : MonoBehaviour
+{
+    
+    private MeshRenderer meshRenderer;
+    private Vector3 headPosition;
+    private Vector3 gazeDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = this.gameObject.GetComponentInChildren<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        headPosition = Camera.main.transform.position;
+        gazeDirection = Camera.main.transform.position;
+        RaycastHit hitInfo;
+        if(Physics.Raycast(headPosition, gazeDirection, out hitInfo) && hitInfo.transform.tag == "interactible"){
+            meshRenderer.enabled = true;
+            this.transform.position = hitInfo.point;
+            
+        }
+        else
+        {
+            meshRenderer.enabled = false;
+        }
+    }
+}
---
+++ Assets/SpeechManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class SpeechManager : MonoBehaviour
+{
+    private KeywordRecognizer recognizeWord = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        keywords.Add("Next Page", () =>
+        {
+             //interact = CanvasGaze.Instance.FocusedObject;
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
