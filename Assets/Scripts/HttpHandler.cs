eae17cebeaba7cc0b5a157fa6d1332db93192a10
ryrabang <ryrabang@my.bcit.ca>
2019-11-29 15:24:31 -0800

    conditional camera click, position of infoDocs, instructional text boxes

---
+++ Assets/Scripts/HttpHandler.cs
@@ -0,0 +1,219 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using SimpleJSON;
+using TMPro;
+using System.Collections.Generic;
+
+/* 
+ * Note: the QR code will have to be the name attribute right now for it to work
+ */
+
+public class HttpHandler : MonoBehaviour
+{
+    //gameObjects to update on canvas
+    public GameObject docImage;
+    public TextMeshProUGUI textInfo;
+    public TextMeshProUGUI title;
+
+    //variables to save data from request 
+    public AudioClip audioClip;
+    public Texture texture;
+    // instance variables
+    private int numberOfSlides;
+    private int currentSlide;
+
+    //list of all RootObjects for one pop up canvas
+    public List<RootObject> slideList =  new List<RootObject>();
+
+
+
+    void Start()
+    {
+        currentSlide = 0;
+        Debug.Log("start starded");
+        Debug.Log("textInfo" + textInfo.text);
+        
+        //StartCoroutine(PostRequest("name", "Hololens"));
+    }
+
+    public void postReq(string str1, string str2)
+    {
+        StartCoroutine(PostRequest(str1, str2));
+    }
+
+    /* PostRequest:
+     * Gets all items from database by name as a json object, saves the data members in RootObjects, and updates slideList list
+     */
+    public IEnumerator PostRequest(string fieldName1, string fieldValue1)
+    {
+        
+        WWWForm form = new WWWForm();
+        form.AddField(fieldName1, fieldValue1);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("https://us-central1-hololens-serverless.cloudfunctions.net/getAllItemsMatchingName", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+                yield return null;
+            }
+            else
+            {
+                //Debug.Log(":\nReceived: " + www.downloadHandler.text);
+                Debug.Log("Form upload complete!");
+
+                //Deserialize JSON 
+                var parsedJSON = JSON.Parse(www.downloadHandler.text);
+                var id = parsedJSON[3]["id"].Value;
+
+                int i = 0;
+                //don't know how to get the total list size so just check if the next entry is an empty string
+                //might wanna change json structure later
+                while(!(parsedJSON[i]["id"].Value.Equals("")))
+                {
+                    Debug.Log("parsed jason");
+                    RootObject r = new RootObject();
+
+                    r.id = parsedJSON[i]["id"].Value;
+                    r.name = parsedJSON[i]["name"].Value;
+                    r.page = parsedJSON[i]["page"].Value;
+                    r.title = parsedJSON[i]["title"].Value;
+                    r.category = parsedJSON[i]["category"].Value;
+                    r.text = parsedJSON[i]["text"].Value;
+                    r.imgurl = parsedJSON[i]["imgurl"].Value;
+                    r.audiourl = parsedJSON[i]["audiourl"].Value;
+
+                    //wait for images and audio to download
+                    yield return StartCoroutine(DownloadImage(r.imgurl, value => r.texture = value));
+                    yield return StartCoroutine(DownloadAudio(r.audiourl, value => r.audioClip = value));
+
+                    r.texture = texture;
+                    r.audioClip = audioClip;
+                    Debug.Log(r.id);
+                    slideList.Add(r);
+                    Debug.Log("testing" + r.title);
+                    //if the first item is done, we can show the view :) 
+                    if(i == 0)
+                    {
+                        currentSlide = 0;
+                        ShowSlide(0);
+                    }
+
+                    i++;
+                }
+                Debug.Log("count" + slideList.Count);
+                numberOfSlides = slideList.Count;
+
+                //testing updating serialized fields
+                    //rawImg.texture = slideList[0].texture;
+                    //audioSource.clip = slideList[0].audioClip;
+                    //Debug.Log(audioSource.clip + " length: " + audioSource.clip.length);
+                    //audioSource.Play();
+            }
+        }
+    }
+
+    /* GetRequest:
+     * Gets all items from database with GET
+     */
+    public IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(": Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log(":\nReceived: " + webRequest.downloadHandler.text);
+
+            }
+        }
+    }
+
+    /* 
+     * Downloads the image from url to texture
+     */
+    public IEnumerator DownloadImage(string MediaUrl, System.Action<Texture2D> result)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(MediaUrl);
+        yield return request.SendWebRequest();
+        if (request.isNetworkError || request.isHttpError)
+            Debug.Log(request.error);
+        else
+            texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+    }
+
+    /* 
+     * Downloads the audio from url to audioClip
+     */
+    public IEnumerator DownloadAudio(string MediaUrl, System.Action<AudioClip> result)
+    {
+        UnityWebRequest request = UnityWebRequestMultimedia.GetAudioClip(MediaUrl, AudioType.WAV);
+        yield return request.SendWebRequest();
+
+        if (request.isNetworkError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            AudioClip clip = DownloadHandlerAudioClip.GetContent(request);
+            //Debug.Log(clip + " length: " + clip.length);
+            if (clip)
+            {
+                audioClip = clip;
+                //audioSource.Play();
+            }
+        }
+    }
+
+    public void ShowSlide(int slideNumber)
+    {
+        Debug.Log("currentSlide:" + currentSlide);
+        Debug.Log("slideNumber:" + slideNumber);
+        Debug.Log("testing" + slideList[slideNumber].text);
+        Debug.Log("text box available" + textInfo.text);
+        textInfo.text = slideList[slideNumber].text;
+        title.text = slideList[slideNumber].title;
+
+        //make texture into sprite programmatically
+        Texture2D tex = (Texture2D) slideList[slideNumber].texture;
+        Sprite sprite = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f), 100.0f);
+        docImage.GetComponent<Image>().sprite = sprite;
+    }
+
+    void OnNextPage()
+    {
+        //check if its the last page. If it is, go back to first page
+        if(currentSlide == numberOfSlides-1)
+        {
+            ShowSlide(0);
+        } else
+        {
+            ShowSlide(currentSlide + 1);
+        }  
+    }
+
+    void OnPreviousPage()
+    {
+        //check if its the first page. If it is, do nothing.
+        if (currentSlide == numberOfSlides - 1)
+        {
+         
+        }
+        else
+        {
+            ShowSlide(currentSlide - 1);
+        }
+    }
+}
+
---
+++ Assets/Scripts/QRCodeScanner.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.WebCam;
+using System.Linq;
+using System;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;
+using Microsoft.MixedReality.Toolkit;
+using UnityEngine.XR.WSA.Input;
+using Microsoft.MixedReality.Toolkit.Input;
+using UnityEngine.Networking;
+
+/**
+ * Creater : Joe
+ * Date : 11/17/2019
+ * 
+ * Description : 
+ * This script is to take photos when a user takes a gesture (tap)
+ * and processe  the image for decoding QR code
+ * If it is successfully decoded, the returned data would be url,
+ * and it sends request for image using the image url
+ * and it sends request for audio file using the audio url
+ * 
+ * P.S : the url is hardcoded now (will be modified later, based on the completion of web admin)
+ */
+
+public class QRCodeScanner : MonoBehaviour, IMixedRealityInputHandler
+{
+    Resolution cameraResolution;
+    PhotoCapture photoCaptureObject = null;
+    public AudioSource audioSource;
+    public Text output;
+    public Text instruction;
+    public Text camaraClicked;
+    public Shader sh;
+    public GameObject infoDoc;
+    private bool qrAvailable = false;
+    private bool enableScan = true;
+
+    void Start()
+    {
+        cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
+        audioSource.Stop();
+        CoreServices.DiagnosticsSystem.ShowDiagnostics = false;
+        CoreServices.DiagnosticsSystem.ShowProfiler = false;
+        // create PhotoCapture object
+        PhotoCapture.CreateAsync(false, delegate (PhotoCapture captureObject)
+        {
+            photoCaptureObject = captureObject;
+            CameraParameters cameraParameters = new CameraParameters();
+            cameraParameters.hologramOpacity = 0.0f;
+            cameraParameters.cameraResolutionWidth = cameraResolution.width;
+            cameraParameters.cameraResolutionHeight = cameraResolution.height;
+            cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
+
+            photoCaptureObject.StartPhotoModeAsync(cameraParameters, delegate (PhotoCapture.PhotoCaptureResult result) { });
+        });
+    }
+
+
+    void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
+    {
+        camaraClicked.text = "camara is clicked >_<";
+        if (enableScan)
+        {
+            List<byte> imageBufferList = new List<byte>();
+
+            photoCaptureFrame.CopyRawImageDataIntoBuffer(imageBufferList);
+
+            string resultStr = "";
+            byte[] byteArr = imageBufferList.ToArray();
+#if !UNITY_EDITOR
+                resultStr = ZxingUwp.Barcode.Read(byteArr, cameraResolution.width, cameraResolution.height);
+#endif
+            enableScan = false;
+            output.text = "Scan result: " + resultStr;
+
+            if (resultStr == "null")
+            {
+                /* failed to decode QR code */
+                instruction.text = "Scan again";
+            }
+            else
+            {
+                //qrAvailable = true;
+                instruction.text = "Scan success";
+                GameObject info = Instantiate(infoDoc, Camera.main.transform.position + Camera.main.transform.forward * 1.66f, Quaternion.identity) as GameObject;
+                info.transform.LookAt(Camera.main.transform);
+                info.transform.LookAt(2 * info.transform.position - Camera.main.transform.position);
+                info.transform.position = new Vector3(info.transform.position.x - 0.5f, info.transform.position.y - 0.2f, info.transform.position.z);
+                info.GetComponent<HttpHandler>().postReq("name", resultStr);
+                /* succeeded to decode QR code */
+            }
+            StartCoroutine(WaitAndPrint(4.4f, "Tap to scan QR code", instruction));
+        }
+    }
+
+    private IEnumerator WaitAndPrint(float waitTime, string str, Text textbox)
+    {
+        yield return new WaitForSeconds(waitTime/2);
+        camaraClicked.text = "";
+        enableScan = true;
+        yield return new WaitForSeconds(waitTime/2);
+        textbox.text = str;
+        output.text = "";
+    }
+
+    // called when user clicked with his finger
+    // takes a photo with hololens camera and tries to decode the image
+    public void OnInputDown(InputEventData eventData)
+    {
+        RaycastHit hit;
+        if (!Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 10f)) photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
+    }
+
+    void OnOpenDocument()
+    {
+        if (qrAvailable == true)
+        {
+            //ensure no other document is open
+            GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("interactible");
+            for (var i = 0; i < gameObjects.Length; i++)
+            {
+                Destroy(gameObjects[i]);
+            }
+            Debug.Log("open document");
+            GameObject info = Instantiate(infoDoc, new Vector3(0, 0, 2), Quaternion.identity) as GameObject;
+            info.transform.parent = GameObject.Find("GameManager").transform;
+            qrAvailable = false;
+        }
+        else
+        {
+            Debug.Log("qr code not available");
+        }
+    }
+
+    public void OnInputUp(InputEventData eventData)
+    {
+    }
+}
